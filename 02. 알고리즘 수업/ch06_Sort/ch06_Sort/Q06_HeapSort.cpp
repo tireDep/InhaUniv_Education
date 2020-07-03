1dd1ad9dda3abfb98e11a65451b54a7b9f860e2a
_tireDep <wing997@naver.com>
2020-07-03 17:31:14 +0900

    [알고리즘 수업 : 힙정렬]
    - 버그 해결

--- 02. 알고리즘 수업/ch06_Sort/ch06_Sort/Q06_HeapSort.cpp
+++ 02. 알고리즘 수업/ch06_Sort/ch06_Sort/Q06_HeapSort.cpp
@@ -6,7 +6,7 @@ using namespace std;
 void PrintArr(int arr[]);
 void HeapSort(int heapArr[], int maxRange);
 
-const int arrSize = 10;
+const int arrSize = 100000;
 
 int main()
 {
@@ -45,44 +45,51 @@ void HeapSort(int heapArr[], int maxRange)
 	int childIndex;
 	int parentIndex;
 	int i = maxRange;
-	
+
 	if (maxRange == 0)
 		return;
-
-	while(i > 0 && i <= maxRange)// for (int i = maxRange; i > 0; i -= 2)
+	while(maxRange > 0)
 	{
-		if (i % 2 == 0)	// 오른쪽 자식
+		while (i > 0 && i <= maxRange)// for (int i = maxRange; i > 0; i -= 2)
 		{
-			childIndex = heapArr[i] > heapArr[i - 1] ? i : i - 1;	// 자식 비교
-			parentIndex = heapArr[childIndex] > heapArr[(i - 1) / 2] ? childIndex : (i - 1) / 2;	// 자식과 부모 비교
-		}
-		else // 왼쪽 자식
-		{
-			childIndex = i;	// 자식 대입
-			parentIndex = heapArr[childIndex] > heapArr[(i - 1) / 2] ? childIndex : (i - 1) / 2;	// 자식과 부모 비교
-		}
+			if (i % 2 == 0)	// 오른쪽 자식
+			{
+				childIndex = heapArr[i] > heapArr[i - 1] ? i : i - 1;	// 자식 비교
+				parentIndex = heapArr[childIndex] > heapArr[(i - 1) / 2] ? childIndex : (i - 1) / 2;	// 자식과 부모 비교
+			}
+			else // 왼쪽 자식
+			{
+				childIndex = i;	// 자식 대입
+				parentIndex = heapArr[childIndex] > heapArr[(i - 1) / 2] ? childIndex : (i - 1) / 2;	// 자식과 부모 비교
+			}
 
-		if (parentIndex != (i - 1) / 2)
-		{
-			temp = heapArr[parentIndex];
-			heapArr[parentIndex] = heapArr[(i - 1) / 2];
-			heapArr[(i - 1) / 2] = temp;
+			if (parentIndex != (i - 1) / 2)
+			{
+				temp = heapArr[parentIndex];
+				heapArr[parentIndex] = heapArr[(i - 1) / 2];
+				heapArr[(i - 1) / 2] = temp;
 
-			if (parentIndex * 2 + 2 >= 0 && parentIndex * 2 + 2 <= maxRange && i % 2 == 0)	// 범위 내에 있고, 현 위치가 오른쪽인 경우(짝수)
-				i = (parentIndex * 2 + 2) + 2;	// 오른쪽 자식으로 i인덱스 이동, -2 대비 +2
-			else if (parentIndex * 2 + 1 >= 0 && parentIndex * 2 + 1 <= maxRange && i % 2 != 0)	// 범위 내에 있고, 현 위치가 왼쪽인 경우(홀수)
-				i = (parentIndex * 2 + 1) + 2;	// 왼쪽 자식으로 i인덱스 이동, -2 대비 + 2
+				if (parentIndex * 2 + 2 >= 0 && parentIndex * 2 + 2 <= maxRange && i % 2 == 0)	// 범위 내에 있고, 현 위치가 오른쪽인 경우(짝수)
+					i = (parentIndex * 2 + 2) + 2;	// 오른쪽 자식으로 i인덱스 이동, -2 대비 +2
+				else if (parentIndex * 2 + 1 >= 0 && parentIndex * 2 + 1 <= maxRange && i % 2 != 0)	// 범위 내에 있고, 현 위치가 왼쪽인 경우(홀수)
+					i = (parentIndex * 2 + 1) + 2;	// 왼쪽 자식으로 i인덱스 이동, -2 대비 + 2
+			}
+
+			if (i % 2 == 0)
+				i -= 2;
+			else
+				i -= 1;
 		}
+		// 힙 완성 후 위치 변경
+		temp = heapArr[maxRange];
+		heapArr[maxRange] = heapArr[0];
+		heapArr[0] = temp;
+		maxRange--;
+		i = maxRange;
 
-		if (i % 2 == 0)
-			i -= 2;
-		else
-			i -= 1;
+		//cout << "<<<";
+		//PrintArr(heapArr);
 	}
-	// 힙 완성 후 위치 변경
-	temp = heapArr[maxRange];
-	heapArr[maxRange] = heapArr[0];
-	heapArr[0] = temp;
 
-	HeapSort(heapArr, maxRange - 1);
+	// HeapSort(heapArr, maxRange - 1);
 }
