c3c65e8e8c4f0c1fd31d49d4a719459512cfe3b0
_tireDep <wing997@naver.com>
2020-07-08 00:00:07 +0900

    [알고리즘 수업 : 기수정렬]

--- 02. 알고리즘 수업/ch06_Sort/ch06_Sort/Q08_RadixSort.cpp
+++ 02. 알고리즘 수업/ch06_Sort/ch06_Sort/Q08_RadixSort.cpp
@@ -5,14 +5,15 @@
 #include<iostream>
 #include<ctime>
 #include<vector>
+#include<queue>
 
 using namespace std;
 
 void PrintArr(int arr[]);
 
 const int arrSize = 10;
-const int maxNum = 9999;
-const int thousand = 1000;
+const int maxNum = 9998;
+const int maxPos = 1000;
 
 int main()
 {
@@ -21,17 +22,27 @@ int main()
 	for (int i = 0; i < arrSize; i++)
 	{
 		radixArr[i] = rand() % maxNum + 1;
-		if (radixArr[i] < 1000)
-			radixArr[i] += 1000;
+		//if (radixArr[i] < 1000)
+		//	radixArr[i] += 1000;
 	}
 
 	PrintArr(radixArr);
 
-	vector<vector<int>> sortArr(arrSize, vector<int>(0));
+/*	vector<vector<int>> sortArr(arrSize, vector<int>(0));
 	for (int i = 0; i < arrSize; i++)
 	{
-		sortArr.at(radixArr[i] / thousand).push_back(radixArr[i]);
+		sortArr.at(radixArr[i] / 1000).push_back(radixArr[i]);
 	}
+
+	for (int i = 0; i < arrSize; i++)
+	{
+		sortArr.at(i).back() / 10 % 10;
+	}
+
+	// / 1000 : 1000 磊府
+	// / 100 % 10 : 100 磊府
+	// / 10 % 10	: 10 磊府
+	// % 10 : 1 磊府
 
 	// PrintVec();
 	// vector<int>::iterator iter;
@@ -65,6 +76,45 @@ int main()
 		}
 		cout << endl;
 	}
+	*/
+
+	queue<int> sortArr[arrSize];
+	int pos = 10;
+	int min = 1;
+	while (min <= maxPos)
+	{
+		for (int i = 0; i < arrSize; i++)
+			sortArr[(radixArr[i] % pos) / min].push(radixArr[i]);
+
+		//cout << "---";
+		//for (int i = 0; i < arrSize;)
+		//{
+		//	if (sortArr[i].size())
+		//	{
+		//		cout << sortArr[i].front() << " ";
+		//		sortArr[i].pop();
+		//	}
+		//	else
+		//		i++;
+		//}
+
+		for (int i = 0, j = 0; i < arrSize;)
+		{
+			if (sortArr[i].size())
+			{
+				radixArr[j++] = sortArr[i].front();
+				sortArr[i].pop();
+			}
+			else
+				i++;
+		}
+		min *= 10;
+		pos *= 10;
+		cout << ">>> ";
+		PrintArr(radixArr);
+	}
+
+	PrintArr(radixArr);
 
 	return 0;
 }
