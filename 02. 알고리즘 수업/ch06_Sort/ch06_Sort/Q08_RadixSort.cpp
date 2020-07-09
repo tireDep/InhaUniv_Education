ad29f3d1f937097e28030a1ad8e4c482aee2102f
_tireDep <wing997@naver.com>
2020-07-10 00:07:59 +0900

    [알고리즘 수업 : 정렬_기수정렬]
    - 기수정렬
    -> 소스코드 정리
    -> 설명 추가
    
    - 계수정렬 설명 수정

--- 02. 알고리즘 수업/ch06_Sort/ch06_Sort/Q08_RadixSort.cpp
+++ 02. 알고리즘 수업/ch06_Sort/ch06_Sort/Q08_RadixSort.cpp
@@ -1,10 +1,54 @@
 /*
-기수정렬 / Radix Sort
--> 재귀함수
+---------------------------------------------------------------
+[기수정렬(Radix Sort)]
+arr : 152, 352, 350, 270, 339, 750, 670, 650, 430, 110
+
+가장 큰 자릿수인 백의 자리부터 해당하는 큐에 삽입
+or 가장 작은 자릿수인 일의 자리부터 순서대로 해당하는 큐에 삽입
+-> 일의 자리부터 시작함
+0 : 350, 270, 750, 670, 650, 430, 110
+1 : 
+2 : 152, 352, 
+3 :
+4 : 
+5 :
+6 : 
+7 : 
+8 : 
+9 : 339
+
+다음 자릿수를 해당하는 큐에 순서대로 삽입
+-> 십의 자리
+0 : 
+1 : 110
+2 : 
+3 : 430, 339
+4 : 
+5 : 350, 750, 650, 152, 352
+6 : 
+7 : 270, 670,
+8 : 
+9 : 
+
+다음 자릿수를 해당하는 큐에 순서대로 삽입
+-> 백의 자리
+0 :
+1 : 110, 152
+2 : 270
+3 : 339, 350, 352
+4 : 430
+5 : 
+6 : 650, 670
+7 : 750
+8 :
+9 :
+
+정렬완료
+---------------------------------------------------------------
 */
+
 #include<iostream>
 #include<ctime>
-#include<vector>
 #include<queue>
 
 using namespace std;
@@ -12,8 +56,6 @@ using namespace std;
 void PrintArr(int arr[]);
 
 const int arrSize = 10;
-const int maxNum = 9998;
-const int maxPos = 1000;
 
 int main()
 {
@@ -21,82 +63,18 @@ int main()
 	srand((unsigned)time(NULL));
 	for (int i = 0; i < arrSize; i++)
 	{
-		radixArr[i] = rand() % maxNum + 1;
-		//if (radixArr[i] < 1000)
-		//	radixArr[i] += 1000;
+		radixArr[i] = rand() % arrSize + 1;
 	}
 
 	PrintArr(radixArr);
 
-/*	vector<vector<int>> sortArr(arrSize, vector<int>(0));
-	for (int i = 0; i < arrSize; i++)
-	{
-		sortArr.at(radixArr[i] / 1000).push_back(radixArr[i]);
-	}
-
-	for (int i = 0; i < arrSize; i++)
-	{
-		sortArr.at(i).back() / 10 % 10;
-	}
-
-	// / 1000 : 1000 자리
-	// / 100 % 10 : 100 자리
-	// / 10 % 10	: 10 자리
-	// % 10 : 1 자리
-
-	// PrintVec();
-	// vector<int>::iterator iter;
-	// for (int i = 0; i < arrSize; i++)
-	// {
-	// 	cout << i << " : ";
-	// 	for (iter = sortArr[i].begin(); iter != sortArr[i].end(); iter++)
-	// 	{
-	// 		cout << *iter << " ";
-	// 	}
-	// 	cout << endl;
-	// }
-	// PrintVec();
-
-	vector<int>::iterator iter;
-	for (int i = 0; i < arrSize; i++)
-	{
-		cout << i << " : ";
-		for (iter = sortArr[i].begin(); iter != sortArr[i].end(); iter++)
-		{
-			cout << *iter << " ";
-		}
-		cout << endl;
-	}
-
-	for (int i = 0; i < arrSize; i++)
-	{
-		for (iter = sortArr[i].begin(); iter != sortArr[i].end(); iter++)
-		{
-			cout << *iter%thousand/100 << " ";
-		}
-		cout << endl;
-	}
-	*/
-
 	queue<int> sortArr[arrSize];
 	int pos = 10;
 	int min = 1;
-	while (min <= maxPos)
+	while (min <= arrSize)
 	{
 		for (int i = 0; i < arrSize; i++)
 			sortArr[(radixArr[i] % pos) / min].push(radixArr[i]);
-
-		//cout << "---";
-		//for (int i = 0; i < arrSize;)
-		//{
-		//	if (sortArr[i].size())
-		//	{
-		//		cout << sortArr[i].front() << " ";
-		//		sortArr[i].pop();
-		//	}
-		//	else
-		//		i++;
-		//}
 
 		for (int i = 0, j = 0; i < arrSize;)
 		{
