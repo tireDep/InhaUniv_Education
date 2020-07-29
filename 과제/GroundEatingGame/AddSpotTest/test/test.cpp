b34b82467f8a90f1164b1a6917d354a71ab12e9b
_tireDep <wing997@naver.com>
2020-07-29 15:53:03 +0900

    [땅따먹기 게임 : 네모 합침 판정]
    - 중간중간 안돌아가는 경우가 존재함

--- 과제/GroundEatingGame/AddSpotTest/test/test.cpp
+++ 과제/GroundEatingGame/AddSpotTest/test/test.cpp
@@ -128,6 +128,10 @@ using std::iterator;
 
 bool AddPolygonSpot_RightBottom(vector<POINT>preRect, vector<POINT>newRect, POINT *test);
 bool AddPolygonSpot_LeftUp(vector<POINT>preRect, vector<POINT>newRect, POINT *test);
+void AddPolygonSpot_Another(vector<POINT>preRect, vector<POINT>newRect, POINT *test);
+int GetArrayCnt(POINT *test);
+
+bool DotInFigure(int x, int y, const vector<POINT> &p);
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
@@ -142,25 +146,25 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 		vector<POINT> rect2;
 
 		POINT temp;
-		// temp = { 100,100 };
-		// rectPos.push_back(temp);
-		// temp = { 200,100 };
-		// rectPos.push_back(temp);
-		// temp = { 200,200 };
-		// rectPos.push_back(temp);
-		// temp = { 100,200 };
-		// rectPos.push_back(temp);
+		temp = { 100,100 };
+		rectPos.push_back(temp);
+		temp = { 200,100 };
+		rectPos.push_back(temp);
+		temp = { 200,200 };
+		rectPos.push_back(temp);
+		temp = { 100,200 };
+		rectPos.push_back(temp);
 
-		//temp = { 200,150 };
-		//rect2.push_back(temp);
-		//temp = { 250,150 };
-		//rect2.push_back(temp);
-		//temp = { 250,250 };
-		//rect2.push_back(temp);
-		//temp = { 150,250 };
-		//rect2.push_back(temp);
-		//temp = { 150,200 };
-		//rect2.push_back(temp);
+		temp = { 200,150 };
+		rect2.push_back(temp);
+		temp = { 250,150 };
+		rect2.push_back(temp);
+		temp = { 250,250 };
+		rect2.push_back(temp);
+		temp = { 150,250 };
+		rect2.push_back(temp);
+		temp = { 150,200 };
+		rect2.push_back(temp);
 
 		//temp = { 200, 200 };
 		//rect2.push_back(temp);
@@ -185,17 +189,29 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 		// temp = { 200, 150 };
 		// rect2.push_back(temp);
 
+		// temp = { 100, 150 };
+		// rect2.push_back(temp);
+		// temp = { 50, 150 };
+		// rect2.push_back(temp);
+		// temp = { 50, 250 };
+		// rect2.push_back(temp);
+		// temp = { 150, 250 };
+		// rect2.push_back(temp);
+		// temp = { 150, 200 };
+		// rect2.push_back(temp);
+
+		// -----------------------------------------------------
 		// 우측, 하단 판정 가능
+		// -----------------------------------------------------
 
-		temp = { 200,100 };
-		rectPos.push_back(temp);
-		temp = { 100,100 };
-		rectPos.push_back(temp);
-		temp = { 100,200 };
-		rectPos.push_back(temp);
-		temp = { 200,200 };
-		rectPos.push_back(temp);
-
+		// temp = { 200,100 };
+		// rectPos.push_back(temp);
+		// temp = { 100,100 };
+		// rectPos.push_back(temp);
+		// temp = { 100,200 };
+		// rectPos.push_back(temp);
+		// temp = { 200,200 };
+		// rectPos.push_back(temp);
 
 		// temp = { 150, 100 };
 		// rect2.push_back(temp);
@@ -217,25 +233,137 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 		// temp = { 100, 200 };
 		// rect2.push_back(temp);
 
-		temp = { 200, 100 };
-		rect2.push_back(temp);
-		temp = { 200, 0 };
-		rect2.push_back(temp);
-		temp = { 100, 0 };
-		rect2.push_back(temp);
-		temp = { 100, 100 };
-		rect2.push_back(temp);
+		// temp = { 200, 100 };
+		// rect2.push_back(temp);
+		// temp = { 200, 0 };
+		// rect2.push_back(temp);
+		// temp = { 100, 0 };
+		// rect2.push_back(temp);
+		// temp = { 100, 100 };
+		// rect2.push_back(temp);
 
-		POINT *test = new POINT[rectPos.size() + rect2.size()];
-		if (AddPolygonSpot_RightBottom(rectPos, rect2, test))
-			Polygon(hdc, test, rectPos.size() + rect2.size());
-		else
+		// -----------------------------------------------------
+		// 좌측, 상단 판정 가능
+		// -----------------------------------------------------
+
+		//temp = { 200,100 };
+		//rectPos.push_back(temp);
+		//temp = { 100,100 };
+		//rectPos.push_back(temp);
+		//temp = { 100,200 };
+		//rectPos.push_back(temp);
+		//temp = { 200,200 };
+		//rectPos.push_back(temp);
+		////temp = { 200,100 };
+		////rectPos.push_back(temp);
+		
+		// temp = { 300, 100 };
+		// rect2.push_back(temp);
+		// temp = { 400, 100 };
+		// rect2.push_back(temp);
+		// temp = { 400, 200 };
+		// rect2.push_back(temp);
+		// temp = { 300, 200 };
+		// rect2.push_back(temp);
+		// //temp = { 300, 100 };
+		// //rect2.push_back(temp);
+
+		//temp = { 400, 400 };
+		//rect2.push_back(temp);
+		//temp = { 500, 400 };
+		//rect2.push_back(temp);
+		//temp = { 500, 500 };
+		//rect2.push_back(temp);
+		//temp = { 400, 500 };
+		//rect2.push_back(temp);
+		////temp = { 300, 100 };
+		////rect2.push_back(temp);
+
+		POINT *test = new POINT[rectPos.size() + rect2.size() + 2];
+		AddPolygonSpot_RightBottom(rectPos, rect2, test);
+		//if (AddPolygonSpot_RightBottom(rectPos, rect2, test))
+		//	Polygon(hdc, test, rectPos.size() + rect2.size());
+		//else
+		//{
+		//	if(AddPolygonSpot_LeftUp(rectPos, rect2, test))
+		//		Polygon(hdc, test, rectPos.size() + rect2.size());
+		//	else
+		//	{
+		//		AddPolygonSpot_Another(rectPos, rect2, test);
+		//		//Polygon(hdc, test, cnt);
+		//	}
+		//}
+
+		vector<POINT>::iterator it;
+		for (it = rectPos.begin(); it < rectPos.end();)
 		{
-			if(AddPolygonSpot_LeftUp(rectPos, rect2, test))
-				Polygon(hdc, test, rectPos.size() + rect2.size());
+			if (it->x != NULL)
+				it = rectPos.erase(it);
 			else
-				Ellipse(hdc, 100, 100, 200, 200);
+				it++;
 		}
+
+		for (int i = 0; i < GetArrayCnt(test); i++)
+		{
+			rectPos.push_back(test[i]);
+		}
+
+
+		vector<POINT>::iterator it2;
+		for (it2 = rect2.begin(); it2 < rect2.end();)
+		{
+			if (it2->x != NULL)
+				it2 = rect2.erase(it2);
+			else
+				it2++;
+		}
+
+		temp = { 150, 250 };
+		rect2.push_back(temp);
+		temp = { 100,250 };
+		rect2.push_back(temp);
+		temp = { 100,200 };
+		rect2.push_back(temp);
+		
+		POINT *test2 = new POINT[rectPos.size() + rect2.size() + 2];
+		AddPolygonSpot_LeftUp(rectPos, rect2, test2);
+
+		vector<POINT>::iterator it3;
+		for (it3 = rectPos.begin(); it3 < rectPos.end();)
+		{
+			if (it3->x != NULL)
+				it3 = rectPos.erase(it3);
+			else
+				it3++;
+		}
+
+		for (int i = 0; i < GetArrayCnt(test2); i++)
+		{
+			rectPos.push_back(test2[i]);
+		}
+		
+		for (int i = 0; i < rectPos.size(); i++)
+		{
+			if (DotInFigure(rectPos[i].x, rectPos[i].y, rectPos))
+				rectPos.erase(rectPos.begin() + i);
+			else
+				continue;
+		}
+
+		Polygon(hdc, &rectPos[0], rectPos.size());
+		// if (AddPolygonSpot_RightBottom(rectPos, rect2, test2))
+		// 	Polygon(hdc, test2, rectPos.size() + rect2.size());
+		// else
+		// {
+		// 	if (AddPolygonSpot_LeftUp(rectPos, rect2, test2))
+		// 		Polygon(hdc, test2, rectPos.size() + rect2.size());
+		// 	else
+		// 	{
+		// 		AddPolygonSpot_Another(rectPos, rect2, test2);
+		// 		Polygon(hdc, test2, GetArrayCnt(test2));
+		// 	}
+		// }
+		delete test;
 
 		EndPaint(hWnd, &ps);
 	}
@@ -394,4 +522,58 @@ bool AddPolygonSpot_LeftUp(vector<POINT>preRect, vector<POINT>newRect, POINT *te
 			test[++i] = preRect[++k];
 		}
 	}
+}
+
+void AddPolygonSpot_Another(vector<POINT>preRect, vector<POINT>newRect, POINT *test)
+{
+	int i = 0;
+	int j = 0;
+	for (i = 0; i < preRect.size(); i++)
+	{
+		test[i].x = preRect[i].x;
+		test[i].y = preRect[i].y;
+	}
+	//test[i].x = preRect[0].x;
+	//test[i++].y = preRect[0].y;
+
+	for (i; i < preRect.size() + newRect.size(); i++)
+	{
+		test[i].x = newRect[j].x;
+		test[i].y = newRect[j++].y;
+	}
+	//test[i].x = newRect[0].x;
+	//test[i].y = newRect[0].y;
+}
+
+int GetArrayCnt(POINT *test)
+{
+	int cnt = 0;
+	int c = 0;
+	while (1)
+	{
+		if (test[c].x >= 0 && test[c].y >= 0)
+			cnt++;
+		else
+			break;
+		c++;
+	}
+	return cnt;
+}
+
+bool DotInFigure(int x, int y, const vector<POINT> &p)
+{
+	int crosses = 0; // (x, y)부터 오른쪽 반직선을 출발해 다각형 p와의 교점의 개수
+	for (int i = 0; i < p.size(); i++)
+	{
+		int j = (i + 1) % p.size();
+		//점 (x, y)가 선분 (p[i], p[j])의 y좌표 사이에 있음
+		if ((p[i].y > y) != (p[j].y > y)) {
+			//atX는 점 B를 지나는 수평선과 선분 (p[i], p[j])의 교점
+			double atX = (p[j].x - p[i].x)*(y - p[i].y) / (p[j].y - p[i].y) + p[i].x;
+			//atX가 오른쪽 반직선과의 교점이 맞으면 교점의 개수를 증가시킨다.
+			if (x < atX)
+				crosses++;
+		}
+	}
+	return crosses % 2 > 0;
 }
