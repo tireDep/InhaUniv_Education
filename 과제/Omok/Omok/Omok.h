b0eda2fdbbe651db09e126a7f51806d99a57f565
_tireDep <wing997@naver.com>
2020-08-13 22:46:56 +0900

    [오목 게임 : 바둑돌 놓기 및 소스코드 정리]
    - 마우스 클릭 위치에 바둑돌 놓기
    -> 좌표 조정
    
    - 소스코드 정리

--- 과제/Omok/Omok/Omok.h
+++ 과제/Omok/Omok/Omok.h
@@ -2,3 +2,15 @@
 
 #include "resource.h"
 
+enum markNum 
+{
+	eMarkLeft = 32, eMarkTop = 20, eMarkRight = 62, eMarkBottom = 50,
+	eMarkDistance = 37, eMarkDiameter = 16
+};
+
+enum mapNum
+{
+	eMapTop = 36, eMapLeft = 47, eMapRight = 714, eMapBottom = 705
+};
+
+#define defArrSize 19
