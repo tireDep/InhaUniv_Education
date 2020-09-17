041b9ac317fb2068db33854bb524a918661462ab
_tireDep <wing997@naver.com>
2020-09-18 01:30:15 +0900

    [행렬 계산_역행렬 구현]
    - 역행렬 구현

--- 04. DirectX/ch01_Math/02_Matrix/02_Matrix/Main.cpp
+++ 04. DirectX/ch01_Math/02_Matrix/02_Matrix/Main.cpp
@@ -11,17 +11,13 @@ int main()
 	cMatrix m1(input);
 	m1.Print();
 
-	// cout << "차수 입력 : ";
-	// cin >> input;
-	// cMatrix m2(input);
-	// m2.Print();
-	// cout << endl;
+	cMatrix m3(input);
+	m3.SetZero();
 
-	// cMatrix m3 = m1*m2;
-	// m3.Print();
-	// m3.Identity(5).Print();
-	m1.Determinent(0);
+	float detNum = m1.Determinent();
+	m3 = m1.Inverse(detNum);
 
+	(m3*m1).Print();
 
 	return 0;
 }
