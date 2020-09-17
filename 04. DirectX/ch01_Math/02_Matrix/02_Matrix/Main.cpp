612390779c6abbd5fa80701d3f288ee24dd79f58
_tireDep <wing997@naver.com>
2020-09-17 12:24:41 +0900

    [Dx : 행렬 계산 구현_형식만]
    - 행렬 계산 구현
    -> 구현 예정

---
+++ 04. DirectX/ch01_Math/02_Matrix/02_Matrix/Main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "cMatrix.h"
+
+using namespace std;
+
+int main()
+{
+
+}
---
+++ 04. DirectX/ch01_Math/02_Matrix/02_Matrix/cMatrix.h
@@ -0,0 +1,53 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <Windows.h>
+
+using namespace std;
+
+class cMatrix
+{
+private:
+	class cRow
+	{
+	private:
+		vector<float> m_vecData;
+
+	public:
+		cRow();
+		cRow(int nDimension);
+		~cRow();
+
+		void Resize(int nDimension);
+		float& operator[](int nIndex);
+	};
+
+public:
+	cMatrix(int nDimension);	// 랜덤 생성
+	~cMatrix();
+
+	void Print();
+	void ReSize(int nDimension);
+	int GetDimension();
+
+	static cMatrix Identity(int nDimension);	// 해당 차수의 단위 행렬
+	cRow& operator[](int nIndex);
+
+	// ==============================================================================
+	bool operator ==(cMatrix &mat);
+	bool operator !=(cMatrix &mat);
+
+	cMatrix operator +(cMatrix &mat);
+	cMatrix operator -(cMatrix &mat);
+	cMatrix operator *(float num);
+	cMatrix operator *(cMatrix &mat);
+	
+	cMatrix Transpose();	// 전치 행렬
+	cMatrix Inverse(OUT float& fDeterminant);	// 역행렬
+
+	float Determinent();
+	cMatrix Adjoint();
+	float Cofactor(int nRow, int nCol);
+	float MinorMatrix(int nRow, int nCol);	// 해당 행렬의 행렬식 구하는 목적
+};
