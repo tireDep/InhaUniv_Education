95895a52c09a91fb37b81fde644902e6af24eecf
_tireDep <wing997@naver.com>
2020-10-07 21:54:36 +0900

    [Dx : .obj 파일 추가, obj 파일 파싱 리팩토링]
    - .obj 파일 추가
    -> gitignore에 의해 추가되지 않았음
    
    - obj 파일 파싱 리팩토링

--- 04. DirectX/DX3D_Texture/DX3D/cObject.cpp
+++ 04. DirectX/DX3D_Texture/DX3D/cObject.cpp
@@ -17,6 +17,11 @@ cObject::~cObject()
 
 void cObject::ReadFileData()
 {
+	ReadObjFile();
+}
+
+void cObject::ReadObjFile()
+{
 	D3DXVECTOR3	readP;
 	vector<D3DXVECTOR3> vecReadP;
 
@@ -28,7 +33,6 @@ void cObject::ReadFileData()
 
 	vector<ST_PNT_VERTEX> vecAddPNT;
 
-	// >> obj
 	bool isCheck = false;
 	ifstream fp;
 	string strRead;
@@ -39,180 +43,161 @@ void cObject::ReadFileData()
 		{
 			getline(fp, strRead);
 
-			// <<
-			if (strstr(strRead.c_str(), ".mtl"))
-			{
-				m_strMtrl = strRead;
-				continue;
-			}
-			// >> mtrl 정보
+			CheckMtrl(strRead);
 
 			if (strstr(strRead.c_str(), "v "))
-			{
-				string token;
-				int pos = 0;
-				string delimeter = " ";
-				
-				// >> readV
-				{
-					pos = strRead.find(delimeter);
-					token = strRead.substr(0, pos);
-					strRead.erase(0, pos + 2);
-
-					pos = strRead.find(delimeter);
-					token = strRead.substr(0, pos);
-					readP.x = stof(token);
-					strRead.erase(0, pos + delimeter.length());
-
-					pos = strRead.find(delimeter);
-					token = strRead.substr(0, pos);
-					readP.y = stof(token);
-					strRead.erase(0, pos + delimeter.length());
-
-					pos = strRead.find(delimeter);
-					token = strRead.substr(0, pos);
-					readP.z = stof(token);
-					strRead.erase(0, pos + delimeter.length());
-
-					vecReadP.push_back(readP);
-				}
-				// << readV
-			}
+				ParseData(strRead, readP, vecReadP);
 
 			if (strstr(strRead.c_str(), "vt "))
-			{
-				string token;
-				int pos = 0;
-				string delimeter = " ";
-
-				// >> readVt
-				{
-					pos = strRead.find(delimeter);
-					token = strRead.substr(0, pos);
-					strRead.erase(0, pos + 2);
-
-					pos = strRead.find(delimeter);
-					token = strRead.substr(0, pos);
-					readT.x = stof(token);
-					strRead.erase(0, pos + delimeter.length());
-
-					pos = strRead.find(delimeter);
-					token = strRead.substr(0, pos);
-					readT.y = stof(token);
-					strRead.erase(0, pos + delimeter.length());
-
-					vecReadT.push_back(readT);
-				}
-				// << readVt
-			}
+				ParseData(strRead, readT, vecReadT);
 
 			if (strstr(strRead.c_str(), "vn "))
-			{
-				string token;
-				int pos = 0;
-				string delimeter = " ";
-
-				// >> readVn
-				{
-					pos = strRead.find(delimeter);
-					token = strRead.substr(0, pos);
-					strRead.erase(0, pos + 2);
-
-					pos = strRead.find(delimeter);
-					token = strRead.substr(0, pos);
-					readN.x = stof(token);
-					strRead.erase(0, pos + delimeter.length());
-
-					pos = strRead.find(delimeter);
-					token = strRead.substr(0, pos);
-					readN.y = stof(token);
-					strRead.erase(0, pos + delimeter.length());
-
-					pos = strRead.find(delimeter);
-					token = strRead.substr(0, pos);
-					readN.z = stof(token);
-					strRead.erase(0, pos + delimeter.length());
-
-					vecReadN.push_back(readN);
-				}
-				// << readVn
-			}
+				ParseData(strRead, readN, vecReadN);
 
 			if (strstr(strRead.c_str(), "g "))
 			{
-				// >> setIndex_fx
-				while (!strstr(strRead.c_str(),"g") || strstr(strRead.c_str(), "g "))
+				// >> index & groudFin
+				while (!strstr(strRead.c_str(), "g") || strstr(strRead.c_str(), "g "))
 				{
 					getline(fp, strRead);
 
-					if (strstr(strRead.c_str(), "usemtl"))
-					{
-						m_strMtrlSet = strRead;
-						continue;
-					}
-					// >> mtrl 정보
-
 					if (strstr(strRead.c_str(), "f "))
-					{
-						// parse & save
-						string token;
-						int pos = 0;
-						string delimeter = " ";
-						int x, y, z;
-
-						while(1)
-						{
-							pos = strRead.find(delimeter);
-							token = strRead.substr(0, pos);
-							strRead.erase(0, pos + delimeter.length());
-							if (0 == pos + 1)
-								break;
-
-							pos = strRead.find("/");
-							token = strRead.substr(0, pos);
-							x = stof(token);
-							strRead.erase(0, pos + delimeter.length());
-
-							pos = strRead.find("/");
-							token = strRead.substr(0, pos);
-							y = stof(token);
-							strRead.erase(0, pos + delimeter.length());
-
-							pos = strRead.find("/");
-							token = strRead.substr(0, pos);
-							z = stof(token);
-
-							ST_PNT_VERTEX temp;
-							temp.p = vecReadP[x - 1];
-							temp.t = vecReadT[y - 1];
-							temp.n = vecReadN[z - 1];
-
-							vecAddPNT.push_back(temp);
-						}
-						// << readVn
-					}
+						ParseIndexData(strRead, vecReadP, vecReadN, vecReadT, vecAddPNT);
 				}
-				// << setIndex
 
-				// >> add & reset
 				m_vecVertex.push_back(vecAddPNT);
-				vecReadP.clear();
-				vecReadT.clear();
-				vecReadN.clear();
-				vecAddPNT.clear();
-				// << add & reset
-			} // if_g objName
+				Reset(vecReadP, vecReadN, vecReadT, vecAddPNT);
+				// << index & groudFin
+			} // if strRead == "g "
 
 		}	// while
+
 	}	// if is_Open()
 	else
 	{
-		MessageBox(g_hWnd, L"Error", L"Error", MB_YESNO);
+		MessageBox(g_hWnd, L"Error : FileOpen", L"Error", MB_YESNO);
 	}
-	// << obj
 
 	MessageBox(g_hWnd, L"fin", L"fin", MB_YESNO);
 
 	fp.close();
+}
 
+void cObject::ParseData(string & strRead, D3DXVECTOR3 & vSet, vector<D3DXVECTOR3> & vecAdd)
+{
+	string token;
+	int pos = 0;
+	string delimeter = " ";
+
+	pos = strRead.find(delimeter);
+	token = strRead.substr(0, pos);
+	strRead.erase(0, pos + 2);
+
+	pos = strRead.find(delimeter);
+	token = strRead.substr(0, pos);
+	vSet.x = stof(token);
+	strRead.erase(0, pos + delimeter.length());
+
+	pos = strRead.find(delimeter);
+	token = strRead.substr(0, pos);
+	vSet.y = stof(token);
+	strRead.erase(0, pos + delimeter.length());
+
+	pos = strRead.find(delimeter);
+	token = strRead.substr(0, pos);
+	vSet.z = stof(token);
+	strRead.erase(0, pos + delimeter.length());
+
+	vecAdd.push_back(vSet);
+	
+	strRead = "\0";
+}
+
+void cObject::ParseData(string & strRead, D3DXVECTOR2 & vSet, vector<D3DXVECTOR2> & vecAdd)
+{
+	string token;
+	int pos = 0;
+	string delimeter = " ";
+
+	pos = strRead.find(delimeter);
+	token = strRead.substr(0, pos);
+	strRead.erase(0, pos + 2);
+
+	pos = strRead.find(delimeter);
+	token = strRead.substr(0, pos);
+	vSet.x = stof(token);
+	strRead.erase(0, pos + delimeter.length());
+
+	pos = strRead.find(delimeter);
+	token = strRead.substr(0, pos);
+	vSet.y = stof(token);
+	strRead.erase(0, pos + delimeter.length());
+
+	vecAdd.push_back(vSet);
+}
+
+void cObject::ParseIndexData(string & strRead, vector<D3DXVECTOR3> & vecP, vector<D3DXVECTOR3>& vecN, vector<D3DXVECTOR2> & vecT, vector<ST_PNT_VERTEX> & vecAdd)
+{
+	string token;
+	int pos = 0;
+	string delimeter = " ";
+	int x, y, z;
+
+	while (1)
+	{
+		pos = strRead.find(delimeter);
+		token = strRead.substr(0, pos);
+		strRead.erase(0, pos + delimeter.length());
+		if (0 == pos + 1)
+		{
+			strRead = "\0";
+			break;
+		}
+
+		pos = strRead.find("/");
+		token = strRead.substr(0, pos);
+		x = stof(token);
+		strRead.erase(0, pos + delimeter.length());
+
+		pos = strRead.find("/");
+		token = strRead.substr(0, pos);
+		y = stof(token);
+		strRead.erase(0, pos + delimeter.length());
+
+		pos = strRead.find("/");
+		token = strRead.substr(0, pos);
+		z = stof(token);
+
+		ST_PNT_VERTEX temp;
+		temp.p = vecP[x - 1];
+		temp.t = vecT[y - 1];
+		temp.n = vecN[z - 1];
+
+		vecAdd.push_back(temp);
+	}
+	// << readVn
+}
+
+void cObject::CheckMtrl(string strRead)
+{
+	if (strstr(strRead.c_str(), ".mtl"))
+	{
+		m_strMtrl = strRead;
+		strRead = "\0";
+	}
+
+	if (strstr(strRead.c_str(), "usemtl"))
+	{
+		m_strMtrlSet = strRead;
+		strRead = "\0";
+	}
+
+}
+
+void cObject::Reset(vector<D3DXVECTOR3> &vecP, vector<D3DXVECTOR3> &vecN, vector<D3DXVECTOR2> &vecT, vector<ST_PNT_VERTEX> &vecAdd)
+{
+	vecP.clear();
+	vecN.clear();
+	vecT.clear();
+	vecAdd.clear();
 }
--- 04. DirectX/DX3D_Texture/DX3D/cObject.h
+++ 04. DirectX/DX3D_Texture/DX3D/cObject.h
@@ -14,4 +14,12 @@ public:
 	~cObject();
 
 	void ReadFileData();
+
+	void ReadObjFile();
+	void CheckMtrl(string strRead);
+	void ParseData(string & strRead, D3DXVECTOR3 & vSet, vector<D3DXVECTOR3> & vecAdd);
+	void ParseData(string & strRead, D3DXVECTOR2 & vSet, vector<D3DXVECTOR2> & vecAdd);
+	void ParseIndexData(string & strRead, vector<D3DXVECTOR3> & vecP, vector<D3DXVECTOR3> & vecN, vector<D3DXVECTOR2> & vecT, vector<ST_PNT_VERTEX> & vecAdd);
+	void Reset(vector<D3DXVECTOR3> &vecP, vector<D3DXVECTOR3> &vecN, vector<D3DXVECTOR2> &vecT, vector<ST_PNT_VERTEX> &vecAdd);
+
 };
