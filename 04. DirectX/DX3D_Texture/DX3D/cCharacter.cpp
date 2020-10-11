06ccc65d9a6d5726eb5f75028ef080e0780a3339
_tireDep <wing997@naver.com>
2020-10-12 00:30:09 +0900

    [Dx : 경로 따라 이동하는 캐릭터]
    - 경로따라 이동하는 캐릭터 구현
    -> 설명 필요

--- 04. DirectX/DX3D_Texture/DX3D/cCharacter.cpp
+++ 04. DirectX/DX3D_Texture/DX3D/cCharacter.cpp
@@ -54,12 +54,40 @@ void cCharacter::Update()
 
 void cCharacter::Update_Sub(vector<ST_PC_VERTEX> path, int &destNum)
 {
-	D3DXMATRIXA16 matR, matT;
-	D3DXMatrixRotationY(&matR, m_fRotY);
+	// D3DXMATRIXA16 matR, matT;
+	// D3DXMatrixRotationY(&matR, m_fRotY);
+	// 
+	// m_vDirection = D3DXVECTOR3(0, 0, 1);
+	// D3DXVec3TransformNormal(&m_vDirection, &m_vDirection, &matR);
+	// D3DXMatrixTranslation(&matT, m_vPosition.x, m_vPosition.y, m_vPosition.z);
+	// 
+	// m_matWorld = matR * matT;
 
-	m_vDirection = D3DXVECTOR3(0, 0, 1);
-	D3DXVec3TransformNormal(&m_vDirection, &m_vDirection, &matR);
+	D3DXMATRIXA16 matR, matT;
+
+	if (sqrt(pow(path[destNum].p.x - m_vPosition.x, 2) + pow(path[destNum].p.z - m_vPosition.z, 2)) < 0.1f)
+	{
+		destNum = destNum + 1 < path.size() ? destNum += 1 : 0;
+
+		D3DXVECTOR3 vUp(0, 1, 0);
+		D3DXMatrixLookAtLH(&matR, &m_vPosition, &path[destNum].p, &vUp);
+		D3DXMatrixTranspose(&matR, &matR);
+
+		D3DXVECTOR3 direction = D3DXVECTOR3(0, 0, 1);
+		D3DXVec3TransformNormal(&direction, &direction, &matR);
+		D3DXVec3Normalize(&direction, &direction);
+
+		m_fRotY += abs(acosf(D3DXVec3Dot(&m_vDirection, &direction)));
+
+		m_vDirection = direction;
+	}
+	else
+	{
+		m_vPosition += m_vDirection * 0.1f;
+	}
+
 	D3DXMatrixTranslation(&matT, m_vPosition.x, m_vPosition.y, m_vPosition.z);
+	D3DXMatrixRotationY(&matR, m_fRotY);
 
 	m_matWorld = matR * matT;
 }
