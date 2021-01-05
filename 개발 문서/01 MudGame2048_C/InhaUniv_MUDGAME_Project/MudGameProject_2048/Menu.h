517bc8cc17dcaaea2a7ada6ae25adb6e18161eec
_tireDep <wing997@naver.com>
2021-01-05 10:19:17 +0900

    [파일정리 : 자료들]
    - 개발문서 자료들 파일정리

--- 개발 문서/01 MudGame2048_C/InhaUniv_MUDGAME_Project/MudGameProject_2048/Menu.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "BasicHeader.h"
-
-void GotoXY(int x, int y);
-void TextColor(int foreground, int background);
-BYTE SelectMenu(BYTE initX, BYTE initY, BYTE gap, BYTE maxMenu, BYTE menuString[][20]);
-void EraseMenu(BYTE initX, BYTE initY, BYTE gap, BYTE maxMenu, BYTE menuString[][20]);
