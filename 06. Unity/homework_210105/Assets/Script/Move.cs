5e11183617f03db8d2c3c79a9cd57aed4a892bc8
_tireDep <wing997@naver.com>
2021-01-06 00:48:37 +0900

    [Unity : 개인 과제_프로펠러 회전_ing]
    - 프로펠러 회전
    -> 큰 프로펠러 완료
    -> 작은 프로펠러 에러?
    => 회전은 되는데 이동시 틀어짐

---
+++ 06. Unity/homework_210105/Assets/Script/Move.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        float fRot = 10.0f;
+        float fSpeed = 10.0f;
+        float moveDelta = fSpeed * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            // this.transform.Translate(Vector3.forward * moveDelta);
+            this.transform.Translate(Vector3.right * moveDelta);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            // Vector3 pos = this.transform.position;
+            // pos.x -= moveDelta;
+            // this.transform.position = pos;
+            // >> 방향에 따라 바뀔 수 있는 듯?
+
+            this.transform.Translate(Vector3.left * moveDelta);
+        }
+
+        else if (Input.GetKey(KeyCode.A))
+        {
+            this.transform.Rotate(Vector3.up * moveDelta * fRot);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            this.transform.rotation *= Quaternion.AngleAxis(moveDelta * fRot * -1, Vector3.up);
+        }
+    }
+}
---
+++ 06. Unity/homework_210105/Assets/Script/SmallPropellerRot.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmallPropellerRot : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    public GameObject target = null;
+    void Update()
+    {
+        float fRot = 10.0f;
+        float fSpeed = 10.0f;
+        float moveDelta = fSpeed * Time.deltaTime;
+
+        if (target != null)
+        {
+            Vector3 pos = target.transform.position;
+
+             this.transform.RotateAround(new Vector3(pos.x, pos.y, pos.z), Vector3.back, moveDelta * fRot);
+        }
+    }
+}
