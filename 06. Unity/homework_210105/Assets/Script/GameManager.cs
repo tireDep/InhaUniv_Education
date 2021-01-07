74ff83e9b03bfcfa7a4b2aada2d9865627778e9d
_tireDep <wing997@naver.com>
2021-01-07 16:36:06 +0900

    [ Unity : 게임매니저, 싱클턴 ]
    - 게임매니저 생성
    -> 오브젝트에 할당하지 말 것!!

---
+++ 06. Unity/homework_210105/Assets/Script/GameManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // >> singleton
+    private static GameManager sInstance;
+
+    public static GameManager Instance
+    {
+        get
+        {
+            if (sInstance == null)
+            {
+                GameObject newGameObject = new GameObject("_GameManager");
+                sInstance = newGameObject.AddComponent<GameManager>();
+            }
+            return sInstance;
+        }
+    }
+    private void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+    // << singleton
+
+    // ====================================
+
+    // private void OnGUI()
+    // {
+    //     ChangeScene();
+    // }
+    // 
+    // public int nSceneID = 0;
+    // private void ChangeScene()
+    // {
+    //     if(nSceneID == 0)
+    //     {
+    //         Debug.Log("씬전환");
+    //     }
+    // }
+}
