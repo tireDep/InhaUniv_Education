a6aca0faa48e64b7b79da13f0c3f8647434aeddf
_tireDep <wing997@naver.com>
2021-01-22 17:27:10 +0900

    [ Unity : 에디터 창 생성 및 편집_치트 입력 창 ]
    - 에디터 창 편집
    -> 치트 입력 창

---
+++ 06. Unity/210120_3D_Terrain/Assets/Editor/CheatWindow.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+public class CheatWindow : EditorWindow
+{
+    //  >> 보통 파일에서 읽어옴
+    string[] cheatList = new string[] { "치트", "골드 생성", "포인트 생성" };
+
+    static int selectIndex = 0;
+    int getInt = 0;
+    string getString = "";
+
+    [MenuItem("Menu/CheatCmd", false, 0)]
+    // >> false / true 는 모양 및 배치에 해당됨
+
+    static public void OpenCheatWindow1()
+    {
+        CheatWindow getWindow = EditorWindow.GetWindow<CheatWindow>(false, "CheatWindow", true);
+    }
+
+    private void OnGUI()
+    {
+        GUILayout.Space(10.0f);
+
+        int getIndex = EditorGUILayout.Popup(selectIndex, cheatList, GUILayout.MaxWidth(200));
+
+        if(selectIndex != getIndex)
+        {
+            selectIndex = getIndex;
+        }
+
+        GUILayout.BeginHorizontal(GUILayout.MaxWidth(300.0f));
+
+        string cheatText = "";
+        // >> 서버 전송 혹은 게임 내에서 입력된 치트 
+
+        if(selectIndex == 0)
+        {
+            GUILayout.Label("치트키 입력", GUILayout.Width(70.0f));
+            getString = EditorGUILayout.TextField(getString, GUILayout.Width(100.0f));
+            cheatText = string.Format("치트키 : {0}", getString);
+        }
+        else if (selectIndex == 1)
+        {
+            GUILayout.Label("골드", GUILayout.Width(70.0f));
+            getString = EditorGUILayout.TextField(getString, GUILayout.Width(100.0f));
+            int.TryParse(getString, out getInt);
+            cheatText = string.Format("골드 : {0}", getInt);
+        }
+        else if (selectIndex == 2)
+        {
+            GUILayout.Label("포인트", GUILayout.Width(70.0f));
+            getString = EditorGUILayout.TextField(getString, GUILayout.Width(100.0f));
+            int.TryParse(getString, out getInt);
+            cheatText = string.Format("포인트 : {0}", getInt);
+        }
+
+        GUILayout.EndHorizontal();
+
+        GUILayout.Space(20.0f);
+        GUILayout.BeginHorizontal(GUILayout.MaxWidth(800.0f));
+        {
+            GUILayout.BeginVertical(GUILayout.MaxWidth(300.0f));
+            {
+                GUILayout.BeginHorizontal(GUILayout.MaxWidth(300.0f));
+                {
+                    if(GUILayout.Button("\n적용\n", GUILayout.Width(100.0f)))
+                    {
+                        if(EditorApplication.isPlaying 
+                          && EditorSceneManager.GetActiveScene().name == "Terrain")
+                         // && EditorApplication.currentScene == "Terrain")
+                        {
+                            getInt = 0;
+                            getString = "";
+                            Debug.Log(cheatText);
+                        }
+                    }
+                }
+                GUILayout.EndHorizontal();
+
+                GUILayout.BeginHorizontal(GUILayout.MaxWidth(300.0f));
+                {
+                    if(GUILayout.Button("\n백그라운드\n실행\n", GUILayout.Width(100.0f)))
+                    {
+                        Application.runInBackground = true;
+                    }
+
+                    if (GUILayout.Button("\n백그라운드\n실행안함\n", GUILayout.Width(100.0f)))
+                    {
+                        Application.runInBackground = false;
+                    }
+
+                    // >> auto 테스트 할 때 필요함
+                    // >> test 용
+                }
+                GUILayout.EndHorizontal();
+            }
+            GUILayout.EndVertical();
+        }
+        GUILayout.EndHorizontal();
+    }
+}
---
+++ 06. Unity/210120_3D_Terrain/Assets/Editor/EditorTest.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+public class EditorTest : MonoBehaviour
+{
+    // >> 툴 제작 등에 사용해 볼 수 있음
+
+    // >> Componet 옆에 추가됨을 확인할 수 있음
+    [MenuItem("Menu/Clear PlayerPrefs")]
+    private static void Clear_PlayerPrefs()
+    {
+        Debug.Log("Clear PlayerPrefs");
+    }
+
+    [MenuItem("Menu/SubMenu/Select")]
+    private static void SubMenu_Selected()
+    {
+        Debug.Log("selected!");
+    }
+
+    // >> 단축키
+    // # : Ctrl
+    // % : Shift
+    // & : Alt
+    [MenuItem("Menu/SubMenu/Hotkey Test 1 %#v")]
+    private static void SubMenu_Hotkey_1()
+    {
+        Debug.Log("Hotkey test : Ctrl + shift + V");
+    }
+
+    [MenuItem("Menu/SubMenu/Hotkey Test 1 &g")]
+    private static void SubMenu_Hotkey_2()
+    {
+        Debug.Log("Hotkey test : Arlt + G");
+    }
+
+
+    [MenuItem("Assets/Load Selected Scene %5")]
+    private static void LoadSelectedScene()
+    {
+        var selected = Selection.activeObject;
+
+        if (EditorApplication.isPlaying)
+            EditorSceneManager.LoadScene(AssetDatabase.GetAssetPath(selected));
+        else
+            EditorSceneManager.OpenScene(AssetDatabase.GetAssetPath(selected));
+            // >> 플레이 상태에서는 동작하지 않음
+    }
+}
+
+
